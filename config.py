50730b303d2ae53de978fed1167b75536fb26f2a
HajraZawwar <130221854+HajraZawwar@users.noreply.github.com>
2025-05-10 18:25:11 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,26 @@
+import os
+
+# MongoDB
+MONGO_URI = ""
+
+# Cassandra
+CASSANDRA_HOSTS = ['']
+CASSANDRA_KEYSPACE = ''
+
+# Neo4j
+NEO4J_URI = ""
+NEO4J_USER = ""
+NEO4J_PASSWORD = ""
+
+
+# S3
+AWS_ACCESS_KEY_ID = ""
+AWS_SECRET_ACCESS_KEY = ""
+S3_BUCKET_NAME = ""
+
+# FAISS (local path to vector index file)
+FAISS_INDEX_PATH = ""
+
+# DynamoDB
+DYNAMODB_REGION = ""
+DYNAMODB_SESSION_TABLE = ""
